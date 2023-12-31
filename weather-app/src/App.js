b9bd7b2320d3622da5b644f49f343309db1135aa
Abdullah Shah <abdullah-shah97@hotmail.com>
2023-12-31 19:53:14 +0000

    Use env variables to connect to the api and retrieve a response data

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import Weather from "./components/Weather";
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const api_key = process.env["REACT_APP_WEATHER_KEY"]
+    const base_url = process.env["REACT_APP_API_URL"]
+    const [coordinates, setCoordinates] = useState(
+        {
+            "lat": 0,
+            "lon": 0
+        }
+    )
+    const [apiData, setApiData] = useState({
+        weather: [{
+            main: ''
+        }]
+    });
+
+    useEffect(() => {
+            const fetchData = async () => {
+                navigator.geolocation.getCurrentPosition(function (position) {
+                    const lat = position.coords.latitude
+                    const lon = position.coords.longitude
+                    setCoordinates({
+                        lat,
+                        lon
+                    })
+                });
+                try {
+                    const response = await fetch(`${base_url}/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${api_key}`)
+                    const data = await response.json()
+                    setApiData(data)
+                    console.log(data)
+                } catch (error) {
+                    console.error(error)
+                }
+            }
+            fetchData()
+        }
+        , [coordinates.lat, coordinates.lon])
+
+    function handleSubmitCoordinate(e) {
+        e.preventDefault();
+        const newCoordinates = {
+            "lon": parseFloat(e.target.lon.value),
+            "lat": parseFloat(e.target.lat.value)
+        }
+        setCoordinates(newCoordinates);
+    }
+
+    return (
+        <div className="App">
+            {(typeof apiData.main != 'undefined') ? (
+                <Weather weatherData={apiData}/>
+            ): (
+                <div></div>
+            )}
+        </div>
+
+    );
 }
 
 export default App;
